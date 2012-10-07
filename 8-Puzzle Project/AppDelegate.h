edd3e5030a4c141ff0204f4831d6541f1bcded92
Shakya <nshakya08@nshakya08m2.local>
2012-10-06 22:31:05 -0500

    Initial Commit

---
+++ 8-Puzzle Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  8-Puzzle Project
+//
+//  Created by Shakya, Neema on 10/6/12.
+//  Copyright (c) 2012 Shakya, Neema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 8-Puzzle Project/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  8-Puzzle Project
+//
+//  Created by Shakya, Neema on 10/6/12.
+//  Copyright (c) 2012 Shakya, Neema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
